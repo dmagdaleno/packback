12a523a472c8b470942a8f218ca591f29885fa4b
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-11 11:50:47 -0300

    commit inicial

---
+++ src/main/java/br/com/boomerang/packback/PackbackApplication.java
@@ -0,0 +1,13 @@
+package br.com.boomerang.packback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackbackApplication.class, args);
+	}
+
+}
