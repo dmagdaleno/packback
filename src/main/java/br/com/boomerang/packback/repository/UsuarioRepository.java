b3ab3252478942bdf4d2a05f645f2d1b573281eb
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-12 21:29:28 -0300

    mofica retorno do método findAll do CrudRepository de usuários

--- src/main/java/br/com/boomerang/packback/repository/UsuarioRepository.java
+++ src/main/java/br/com/boomerang/packback/repository/UsuarioRepository.java
@@ -3,5 +3,10 @@ package br.com.boomerang.packback.repository;
 import br.com.boomerang.packback.domain.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+    @Override
+    Collection<Usuario> findAll();
 }
