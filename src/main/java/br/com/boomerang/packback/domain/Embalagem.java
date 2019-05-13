3e6ddf525d22735de710ecc2d7c2033b65bff77b
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-13 00:54:45 -0300

    nova entidade

---
+++ src/main/java/br/com/boomerang/packback/domain/Embalagem.java
@@ -0,0 +1,9 @@
+package br.com.boomerang.packback.domain;
+
+public class Embalagem {
+    private Long id;
+    private String descricao;
+    private Double volume;
+    private Double peso;
+    private Double pontos;
+}
