04d2e6a8580715a61f4c2de62d076d46a2879661
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-13 22:52:45 -0300

    corrige mapeamento do rest de usuarios

--- src/main/java/br/com/boomerang/packback/controller/UsuarioController.java
+++ src/main/java/br/com/boomerang/packback/controller/UsuarioController.java
@@ -7,11 +7,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
-@RestController("/usuarios")
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     private static final Logger log = LoggerFactory.getLogger(UsuarioController.class);
