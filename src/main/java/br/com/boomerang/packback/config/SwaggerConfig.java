483f25dc8324370e94300981167036a98f96e796
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-14 08:34:40 -0300

    configura swagger para apresentar a api de movimentações

--- src/main/java/br/com/boomerang/packback/config/SwaggerConfig.java
+++ src/main/java/br/com/boomerang/packback/config/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.boomerang.packback.controller"))
-                .paths(regex("(/usuarios.*|/embalagens.*)"))
+                .paths(regex("(/usuarios.*|/embalagens.*|/movimentacoes.*)"))
                 .build()
                 .apiInfo(metaData());
     }
