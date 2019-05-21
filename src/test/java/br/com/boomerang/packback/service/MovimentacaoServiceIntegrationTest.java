85e14df0be4d2c9dcb1c53138aac0f0d75ab1d31
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-20 22:07:14 -0300

    testa método que registra a movimentação

---
+++ src/test/java/br/com/boomerang/packback/service/MovimentacaoServiceIntegrationTest.java
@@ -0,0 +1,53 @@
+package br.com.boomerang.packback.service;
+
+import br.com.boomerang.packback.domain.Embalagem;
+import br.com.boomerang.packback.domain.Usuario;
+import br.com.boomerang.packback.repository.InicializadorDeBanco;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovimentacaoServiceIntegrationTest {
+
+    @Autowired
+    private MovimentacaoService service;
+
+    @Autowired
+    private InicializadorDeBanco init;
+
+    private Usuario consumidor;
+
+    private Usuario produtor;
+
+    private Embalagem embalagem;
+
+    @Before
+    public void setup() {
+        var objects = init.inicializaBase();
+        consumidor = (Usuario) objects.get(0);
+        produtor = (Usuario) objects.get(1);
+        embalagem = (Embalagem) objects.get(2);
+    }
+
+    @Test
+    @Transactional
+    public void deveCadastrarMovimentacaoEAtribuirPontosAoUsuarioConsumidor() {
+        assertThat(consumidor.getPontos()).isEqualTo(.0);
+
+        var movimentacaoSalva = service.movimenta(embalagem, consumidor, produtor);
+
+        var movimentacao = service.buscaPorId(movimentacaoSalva.getId());
+
+        assertThat(movimentacao.getUsuarioOrigem().getPontos()).isEqualTo(3150.0);
+    }
+
+}
