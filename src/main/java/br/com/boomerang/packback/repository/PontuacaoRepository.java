5b605200a29e906994bb83e4a7d1756289403d37
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-23 23:19:25 -0300

    busca pontuações por usuário no repositório

--- src/main/java/br/com/boomerang/packback/repository/PontuacaoRepository.java
+++ src/main/java/br/com/boomerang/packback/repository/PontuacaoRepository.java
@@ -3,5 +3,10 @@ package br.com.boomerang.packback.repository;
 import br.com.boomerang.packback.domain.Pontuacao;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PontuacaoRepository extends CrudRepository<Pontuacao, Long> {
+
+    List<Pontuacao> findAllByUsuarioId(Long id);
+
 }
