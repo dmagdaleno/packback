097b51eed0f8efcb25385384b493bd77dfb01c7a
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-08-25 21:55:47 -0300

    melhora log do fluxo de login

--- src/main/java/br/com/boomerang/packback/service/LoginService.java
+++ src/main/java/br/com/boomerang/packback/service/LoginService.java
@@ -4,12 +4,16 @@ import br.com.boomerang.packback.domain.Autenticacao;
 import br.com.boomerang.packback.domain.Login;
 import br.com.boomerang.packback.domain.Usuario;
 import br.com.boomerang.packback.repository.LoginRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class LoginService {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginService.class);
 
     private final UsuarioService usuarioService;
     private final LoginRepository loginRepository;
@@ -22,17 +26,25 @@ public class LoginService {
     public Optional<Usuario> autentica(Autenticacao autenticacao) {
         var usuario = usuarioService.buscaPorEmail(autenticacao.getEmail());
 
-        if(usuario.isEmpty())
-            throw new IllegalArgumentException("E-mail não encontrado");
+        if(usuario.isEmpty()) {
+            log.info("E-mail {} não encontrado", autenticacao.getEmail());
+            return Optional.empty();
+        }
 
         Optional<Login> login = loginRepository.findById(usuario.get().getLogin().getId());
 
-        if(login.isEmpty())
-            throw new IllegalArgumentException("Autenticação inválida");
-
-        if(autenticacao.getSenha().equals(login.get().getSenha()))
-            return usuario;
-        else
+        if(login.isEmpty()){
+            log.info("Autenticação inválida para o email {}", autenticacao.getEmail());
             return Optional.empty();
+        }
+
+        if(autenticacao.getSenha().equals(login.get().getSenha())) {
+            log.info("Usuário {} autenticado com sucesso", autenticacao.getEmail());
+            return usuario;
+        }
+        else {
+            log.info("Senha incorreta para o email {}", autenticacao.getEmail());
+            return Optional.empty();
+        }
     }
 }
